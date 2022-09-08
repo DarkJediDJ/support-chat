11555941925922aef45a7aa8f75828bf9c7dbbae
DarkJediDJ <xintut@mail.ru>
2022-09-08 15:52:06 +0200

    feat(sender):added handler and server

---
+++ sender-serice/api/server.go
@@ -0,0 +1,25 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/DarkJediDJ/support-chat/sender-serice/api/sender"
+	"github.com/gorilla/mux"
+	"github.com/segmentio/kafka-go"
+)
+
+type App struct {
+	Router *mux.Router
+}
+
+func (a *App) New(conn *kafka.Conn) {
+
+	myRouter := mux.NewRouter().StrictSlash(false)
+	myRouter.HandleFunc("/", sender.Init(conn).Send).Methods("POST")
+	a.Router = myRouter
+}
+
+func (a *App) Run(addr string) {
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
---
+++ sender-serice/cmd/sender-service/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	server "github.com/DarkJediDJ/support-chat/sender-serice/api"
+	"github.com/segmentio/kafka-go"
+)
+
+const addr = ":8080"
+
+func main() {
+	a := server.App{}
+
+	conn, err := kafka.DialLeader(context.Background(), "tcp", "localhost:9092", "support_chat", 0)
+	if err != nil {
+		if err != nil {
+			log.Printf("An Error Occured %v", err)
+		}
+	}
+
+	a.New(conn)
+
+	a.Run(addr)
+}
